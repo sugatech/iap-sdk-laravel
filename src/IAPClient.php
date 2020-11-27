5418ddec19248827bebc2b20d5d8ca98d19a9f04
Quang Hung <ngochung.quang.1310@gmail.com>
2020-11-27 11:07:29 +0700

    [#5] Payment product (Add management product)

--- src/IAPClient.php
+++ src/IAPClient.php
@@ -36,6 +36,7 @@ class IAPClient
     /**
      * @param callable $handler
      * @return Response
+     * @throws \Illuminate\Http\Client\RequestException
      */
     private function request($handler)
     {
@@ -68,6 +69,7 @@ class IAPClient
      * @param string $type
      * @param string|array $receipt
      * @return array
+     * @throws \Illuminate\Http\Client\RequestException
      */
     public function createPurchase($owner, $productId, $type, $receipt)
     {
@@ -82,6 +84,87 @@ class IAPClient
             return $request->asJson()
                 ->post($this->getUrl('/purchases'), $params);
         })
+            ->throw()
+            ->json();
+    }
+
+    /**
+     * @param array $params ['page' => 1, 'limit' => 10, 'sort' => 'id', 'dir' => 'asc']
+     * @return array[]
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function getProducts($params = [])
+    {
+        return $this->request(function (PendingRequest $request) use ($params) {
+            return $request->get(
+                $this->getUrl('/products'),
+                $params
+            );
+        })
+            ->throw()
+            ->json();
+    }
+
+    /**
+     * @param int $productId
+     * @return array
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function getProduct($productId)
+    {
+        return $this->request(function (PendingRequest $request) use ($productId) {
+            return $request->get(
+                $this->getUrl('/products/' . $productId)
+            );
+        })
+            ->throw()
+            ->json();
+    }
+
+    /**
+     * @param string $storeKey
+     * @param string $name
+     * @param string $imageUrl
+     * @param float $price
+     * @param float $value
+     * @return array
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function createProduct($storeKey, $name, $imageUrl, $price, $value)
+    {
+        $params = [
+            'store_key' => $storeKey,
+            'name' => $name,
+            'image_url' => $imageUrl,
+            'price' => $price,
+            'value' => $value
+        ];
+
+        return $this->request(function (PendingRequest $request) use ($params) {
+            return $request->post(
+                $this->getUrl('/products'),
+                $params
+            );
+        })
+            ->throw()
+            ->json();
+    }
+
+    /**
+     * @param int $productId
+     * @param array $params ['name' => '', 'price' => '', 'value' => '']
+     * @return array
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function updateProduct($productId, $params = [])
+    {
+        return $this->request(function (PendingRequest $request) use ($productId, $params) {
+            return $request->put(
+                $this->getUrl('/products/' . $productId),
+                $params
+            );
+        })
+            ->throw()
             ->json();
     }
 }
